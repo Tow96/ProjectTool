9f7eda9e94737dc82318fbc1e1971874d60437cd
Jose Tow <jose.towe@gmail.com>
2023-02-27 20:52:56 -0600

    Added Swagger UI

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,11 +7,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { PidWinstonLogger } from '@shared/logger';
-
 // Modules
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // Services
 import { ConfigService } from '@nestjs/config';
 import { TrimPipe } from './pipes';
@@ -30,6 +29,15 @@ async function bootstrap() {
 
   // Enables CORS only if development
   if (configService.get('NODE_ENV') === 'development') app.enableCors();
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Project Manager Tool')
+    .setDescription('Another somewhat overkill app to organize project folders')
+    .setVersion('1.0')
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDoc);
 
   await app.listen(configService.get('PORT'));
 }
